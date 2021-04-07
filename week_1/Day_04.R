bb7f028ff50f1e8195207b7d132a94268eeed1df
Wilebaldo Santana Hernández <wisahe@gmail.com>
2021-04-06 22:16:41 -0600

    Day_04.R file.rename fixed

--- week_1/Day_04.R
+++ week_1/Day_04.R
@@ -2,10 +2,12 @@
 # Semana 1: Comparisons
 # Día 4: Magical
 # Los más grandes magos e ilusionistas de la época moderna
+# Fuentes:
+# 1. https://totallyhistory.com/biography/famous-magicians/
+# 2. https://www.google.com/
 
 # Cargamos las librerías ####
 library(lares)
-library(ggplot2)
 library(tidyverse)
 
 magos <- read_csv("data/interim//magician.csv") %>% 
@@ -22,8 +24,7 @@ plot_timeline(
   group = magos$activity,
   save = TRUE,
   title = "Grandes magos e ilusionistas de la historia",
-  subtitle = "Elaborado por Will Santana (@willshDev)",
-  subdir = "reports/figures/"
+  subtitle = "Elaborado por Will Santana (@willshDev)"
 )
 
-file.rename("reports/figures/cv_timeline.png", "reports/figures/day_04.png")
+file.rename("cv_timeline.png", "reports/figures/day_04.png")
